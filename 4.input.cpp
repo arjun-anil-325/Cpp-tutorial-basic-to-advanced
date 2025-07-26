be624d2a95f330737cb46e18beea13300ac63619
arjun-anil-325 <2002arjunanil@gmail.com>
2025-07-26 15:32:19 +0530

    Removed duplicate files after restructuring

--- 4.input.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int age;
-    cout << "Enter your age : ";
-    cin >> age; // cin is used to take input from the user
-    cout << "Your age is "<<age<<endl;
-    return 0;
-}
